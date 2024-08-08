9345db7558bc1a496b1f220a5904c9bbe50b61f3
xsylll <sankayla13@gmail.com>
2024-08-07 23:53:47 -0400

    finished day 23. testing w/ environemnt modifiers, views as properties

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,7 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // add ViewBuilder bc swift doesnt do it automatically
+    @ViewBuilder var saying1: some View { // computed property
+        Text("when life gives you lemons")
+    }
+    
+    let saying2 = Text("make lemonade")
+    
+    @State private var redText = false
+    
     var body: some View {
+        VStack {
+            saying1
+                .foregroundStyle(.purple)
+            saying2
+        }
+        
+        
+        Button("red or blue? that's the question") {
+            redText.toggle()
+        }
+        .foregroundStyle(redText ? .red : .blue)
+        // use ternary operator when able
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
@@ -18,9 +40,7 @@ struct ContentView: View {
         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
         .background(.pink)
         
-        Button("hello world") {
-            print(type(of: self.body))
-        }
+        Text("hello world")
         .frame(width: 200, height: 200)
         .background(.red)
         .padding()
@@ -30,6 +50,18 @@ struct ContentView: View {
         .padding()
         .background(.orange)
         
+        VStack {
+            Text("one")
+                .blur(radius: 3)
+                .font(.largeTitle)
+            // blur is regular modifier
+            // font is environment modifier
+            Text("two")
+            Text("three")
+            Text("four")
+        }
+        .blur(radius: 1)
+        .font(.title)
     }
 }
 
